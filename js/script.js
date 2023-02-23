56c1264ca23b253bd49dfc8451ebd87e7e90f361
pierrephz <pierre.philouze@thethingsindustries.com>
2023-02-23 15:01:22 +0100

    add initial website

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const hamburgerToggler = document.querySelector(".hamburger");
+const navLinksContainer = document.querySelector(".navlinks-container");
+
+const toggleNav = () => {
+  if (window.innerWidth > 750) return;
+  hamburgerToggler.classList.toggle("open");
+
+  const ariaToggler =
+    hamburgerToggler.getAttribute("aria-expended") === "true"
+      ? "false"
+      : "true";
+  hamburgerToggler.setAttribute("aria-expanded", ariaToggler);
+
+  navLinksContainer.classList.toggle("open");
+
+  document.body.classList.toggle("no-scroll");
+};
+hamburgerToggler.addEventListener("click", toggleNav);
+navLinksContainer.addEventListener("click", toggleNav);
+
+window.addEventListener("resize", function () {
+  if (window.innerWidth > 750) {
+    hamburgerToggler.classList.remove("open");
+    navLinksContainer.classList.remove("open");
+  }
+});
+
+new ResizeObserver((entries) => {
+  if (entries[0].contentRect.width <= 750) {
+    navLinksContainer.style.transition = "transform .2s ease-out";
+  } else {
+    navLinksContainer.style.transition = "none";
+  }
+}).observe(document.body);
