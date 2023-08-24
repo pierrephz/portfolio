25d64b3909c51b7bf1aa1832908311e1a8aa438b
pierrephz <pierre.philouze@thethingsindustries.com>
2023-08-24 13:19:37 +0200

    Try fixing double tap issue on mobile

--- js/script-scroll.js
+++ js/script-scroll.js
@@ -1,19 +1,3 @@
-// window.addEventListener("scroll", reveal);
-
-// function reveal() {
-//   var reveals = document.querySelectorAll(".reveal");
-//   for (var i = 0; i < reveals.length; i++) {
-//     var windowheight = window.innerHeight;
-//     var revealtop = reveals[i].getBoundingClientRect().top;
-//     var revealpoint = 5;
-//     if (revealtop < windowheight - revealpoint) {
-//       reveals[i].classList.add("active");
-//     } else {
-//       reveals[i].classList.remove("active");
-//     }
-//   }
-// }
-
 // Define the reveal function
 function reveal() {
   var reveals = document.querySelectorAll(".reveal");
@@ -31,18 +15,26 @@ function reveal() {
 
 // Function to manage the event listener
 function manageEventListener() {
+  var reveals = document.querySelectorAll(".reveal");
+
   if (window.matchMedia("(max-width: 700px)").matches) {
     /* The viewport is less than, or equal to, 700 pixels wide */
-    // Remove the scroll event listener
+    // Add 'active' class to all '.reveal' elements and remove the scroll event listener
+    reveals.forEach(function (reveal) {
+      reveal.classList.add("active");
+    });
     window.removeEventListener("scroll", reveal);
   } else {
     /* The viewport is greater than 700 pixels wide */
-    // Add the scroll event listener
+    // Remove 'active' class from all '.reveal' elements and add back the scroll event listener
+    reveals.forEach(function (reveal) {
+      reveal.classList.remove("active");
+    });
     window.addEventListener("scroll", reveal);
   }
 }
 
-// Call manageEventListener on page load
+// Call manageEventListener immediately when the script loads
 manageEventListener();
 
 // Call manageEventListener on viewport resize
